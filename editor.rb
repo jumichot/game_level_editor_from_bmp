4ca8df54f49cf9dc4cc7bb3fdf9237ad1c9a35dd
Julien Michot <julien@captive.fr>
2013-06-22 17:51:14 +0200

    On travail sur une classe Image pour le moment

--- editor.rb
+++ editor.rb
@@ -2,14 +2,14 @@ require "rmagick"
 require "minitest/autorun"
 
 module LevelEditor
-  class Level
+  class Image
     attr_reader :original_image
 
     def initialize(image_path)
       @original_image = Magick::Image::read(image_path).first
     end
 
-    def length
+    def width
       @original_image.columns
     end
   end
@@ -17,19 +17,15 @@ end
 
 describe "Use case on a simple image 6x3" do
   before do
-    @editor = LevelEditor::Level.new("test2.bmp")
+    @image = LevelEditor::Image.new("test_case1.bmp")
   end
 
   it "must have an Magick::Image instance attribute" do
-    @editor.original_image.class.must_equal Magick::Image
+    @image.original_image.class.must_equal Magick::Image
   end
 
   it "can retrieve the length in meter of the level" do
-    @editor.length.must_equal(6)
+    @image.width.must_equal(6)
   end
 
 end
-
-
-
-
