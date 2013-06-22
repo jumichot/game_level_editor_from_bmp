b0b714ffe3a928e1ecb45e4cd021dbcc43750e72
Julien Michot <julien@captive.fr>
2013-06-22 19:07:06 +0200

    Sauvegarde l'image sous forme d'un tableau de pixel

--- editor.rb
+++ editor.rb
@@ -3,10 +3,11 @@ require "minitest/autorun"
 
 module LevelEditor
   class Image
-    attr_reader :original_image
+    attr_reader :original_image, :pixels
 
     def initialize(image_path)
       @original_image = Magick::Image::read(image_path).first
+      create_pixels_array
     end
 
     def width
@@ -17,10 +18,16 @@ module LevelEditor
       @original_image.rows
     end
 
+    def create_pixels_array
+      @pixels = []
+      @original_image.each_pixel do |pixel, col, row|
+        @pixels << pixel
+      end
+    end
   end
 end
 
-describe "Use case on a simple image 6x3" do
+describe LevelEditor::Image do
   before do
     @image = LevelEditor::Image.new("test_case1.bmp")
   end
@@ -35,5 +42,10 @@ describe "Use case on a simple image 6x3" do
 
   it "can retrieve his heigth" do
     @image.heigth.must_equal(3)
+  end
+
+  it "convert the original image into an array of pixels" do
+    @image.pixels.must_be_instance_of(Array)
+    @image.pixels.size.must_equal(18)
   end
 end
