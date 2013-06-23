ef173dfc71ca252889a875a84b957c360ce44995
Julien Michot <julien@captive.fr>
2013-06-23 21:54:33 +0200

    Le lineManager peut scanner des lignes et les ajouter à son tableau de lignes

--- lib/level_editor/elements/line_manager.rb
+++ lib/level_editor/elements/line_manager.rb
@@ -1,11 +1,30 @@
 module LevelEditor
   class LineManager
-    attr_reader :image_interroger
+    attr_reader :image_interroger, :lines
 
     COLOR = "black"
 
     def initialize(image_interroger)
       @image_interroger = image_interroger
+      @lines =  Hash.new{|hash, key| hash[key] = []}
+      @lines_array =  Hash.new{|hash, key| hash[key] = []}
+    end
+
+    def scan_line(x,y)
+      return if @image_interroger.get_color(x,y) != COLOR
+      [:horizontal,:vertical].each do |direction|
+        consecutive_pixels_array = @image_interroger.find_all_pixels_on_axis(direction,x,y)
+        add_line(consecutive_pixels_array) if consecutive_pixels_array.count > 1
+      end
+    end
+
+    def add_line(consecutive_pixels_array)
+      all_x, all_y = consecutive_pixels_array.transpose
+      line =  Line.new(all_x.min,all_y.min,all_x.max,all_y.max)
+      unless @lines_array[line.direction].include? line.to_a
+        @lines[line.direction] << line
+        @lines_array[line.direction] << line.to_a
+      end
     end
   end
 end
--- test/level_editor/element/line_manager_test.rb
+++ test/level_editor/element/line_manager_test.rb
@@ -1,9 +1,54 @@
 require "minitest_helper"
 
 describe LevelEditor::LineManager do
-  it "has an instance attribute of level editor image" do
-    img = LevelEditor::ImageInterroger.new("images/test_case1.bmp")
-    @manager = LevelEditor::LineManager.new(img)
-    @manager.image_interroger.must_equal(img)
+
+  describe "with two crossing lines" do
+    before do
+      @image_interroger = LevelEditor::ImageInterroger.new("images/test_case_3.bmp")
+      @line_manager = LevelEditor::LineManager.new(@image_interroger)
+    end
+
+    it "has an instance attribute of level editor image" do
+      @line_manager.image_interroger.must_equal(@image_interroger)
+    end
+
+    it "build arrays of line from arrays of consecutive vertical pixels" do
+      horizontal_consecutive_pixels = @image_interroger.find_all_pixels_on_axis(:horizontal,1,1)
+      @line_manager.add_line(horizontal_consecutive_pixels)
+      @line_manager.lines[:horizontal].first.to_a.must_equal([0,3,1])
+      @line_manager.lines[:horizontal].size.must_equal(1)
+    end
+
+    it "build arrays of line from arrays of consecutive horizontal pixels" do
+      vertical_consecutive_pixels = @image_interroger.find_all_pixels_on_axis(:vertical,3,1)
+      @line_manager.add_line(vertical_consecutive_pixels)
+      @line_manager.lines[:vertical].first.to_a.must_equal([3,0,1])
+      @line_manager.lines[:vertical].size.must_equal(1)
+    end
+
+    it "can scan line without doublon" do
+      @line_manager.scan_line(0,1)
+      @line_manager.lines[:horizontal].count.must_equal(1)
+      @line_manager.lines[:vertical].must_be_empty
+      @line_manager.scan_line(1,1)
+      @line_manager.lines[:horizontal].count.must_equal(1)
+      @line_manager.lines[:vertical].must_be_empty
+    end
+
+    it "doesnt count the isolated 1x1 block" do
+      @line_manager.scan_line(4,3)
+      lines_must_be_empty
+    end
+
+    it "doesnt count the other color block" do
+      @line_manager.scan_line(7,4)
+      lines_must_be_empty
+    end
+
+    def lines_must_be_empty
+      @line_manager.lines[:horizontal].must_be_empty
+      @line_manager.lines[:vertical].must_be_empty
+    end
   end
+
 end
